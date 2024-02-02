82ae099b2cbc436f07ec6bc073ea41f69fc924dd
rasikhkp <raxiest@gmail.com>
2024-02-02 13:21:40 +0700

    fix: fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import Unfold from "./Pages/Unfold";
-import Home from "./Pages/Home";
-import SimpleAnimation from "./components/SimpleAnimation";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Ngetes from "./components/Ngetes";
 
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/unfold" element={<Unfold />} />
-				<Route path="/simpleanimation" element={<SimpleAnimation />} />
-				<Route path="/ngetes" element={<Ngetes />} />
-			</Routes>
-		</Router>
-	);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Unfold />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
